d0623da573a3852b733341c6f352d05ad4d4732c
Alan Tran <alantran8881@gmail.com>
2022-10-12 21:40:45 -0700

    Commenting and last minute adjustments
    Comments to explain functions, and do last minute adjustments to the code

--- CalculatorProjectJava/src/Calculator.java
+++ CalculatorProjectJava/src/Calculator.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 public class Calculator{
     // Create a custom eval function, which is a version of a recursive descent parser
-    //It is able to take a user submitted math string and evaluate it
+    //  It is able to take a user submitted math string and evaluate it
+    //  The majority of the eval function was taken from this link:
+    //  https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form/26227947#26227947
+    //  and was modified to contain and work with more functions
     public static double eval(final String str) {
         return new Object() {
             int pos = -1, ch;
+
 
             void nextChar() {
                 ch = (++pos < str.length()) ? str.charAt(pos) : -1;
@@ -31,6 +35,8 @@ public class Calculator{
                 return x;
             }
 
+            // To parse the expression based on the operators given and by precedence
+
             // Grammar:
             // expression = term | expression `+` term | expression `-` term
             //        term = pow | term `*` pow | term `/` pow
@@ -38,9 +44,10 @@ public class Calculator{
             // factor = `+` pow  | `-` pow | `(` expression `)`
             //        | number | functionName factor
 
+            // To parse addition and subtraction
             double parseExpression() {
                 double x = parseTerm();
-                for (; ; ) {
+                while(true) {
                     if (eat('+')) { //addition
                         x += parseTerm();
                     } else if (eat('-')) { //subtraction
@@ -51,9 +58,10 @@ public class Calculator{
                 }
             }
 
+            // To parse multiplication and division
             double parseTerm() {
                 double x = parsePow();
-                for (; ; ) {
+                while(true) {
                     if (eat('*')) { //multiplication
                         x *= parsePow();
                     } else if (eat('/')) { //division
@@ -64,9 +72,10 @@ public class Calculator{
                 }
             }
 
+            // To parse exponents
             double parsePow() {
                 double x = parseFactor();
-                for (; ; ) {
+                while(true) {
                     if (eat('^')) {
                         x = Math.pow(x, parseFactor());
                     } else {
@@ -76,12 +85,14 @@ public class Calculator{
             }
 
 
+            // The general parse function, which also sets up unary and binary + and -, while also setting up
+            // the parentheses precedence
             double parseFactor() {
                 if (eat('+')) { //unary plus
                     return parsePow(); //unary plus
                 }
                 if (eat('-')) { //unary minus
-                    return -parsePow();
+                    return -parsePow(); // unary minus
                 }
                 double x = 0;
                 int startPos = this.pos;
@@ -99,6 +110,7 @@ public class Calculator{
                     }
                     String func = str.substring(startPos, this.pos);
                     x = parseFactor();
+                    // All the equals func can be to execute the associated trigonometric or logarithmic function
                     if (func.equals("sqrt")) {
                         x = Math.sqrt(x);
                     } else if (func.equals("log")) {
@@ -111,6 +123,8 @@ public class Calculator{
                         x = Math.cos(Math.toRadians(x));
                     } else if (func.equals("tan")) {
                         x = Math.tan(Math.toRadians(x));
+                    } else if (func.equals("cot")) {
+                        x = 1.0 / Math.tan(Math.toRadians(x));
                     } else if (func.equals("sinh")) {
                         x = Math.sinh(x);
                     } else if (func.equals("cosh")) {
@@ -146,7 +160,7 @@ public class Calculator{
             System.out.println("Please input the expression you would like solved. Or Type 'exit' to close the program.");
             userInput = scanner.nextLine();
             try {
-                System.out.println("Answer is:" + eval(userInput));
+                System.out.println("Answer is: " + eval(userInput));
             }
             catch (RuntimeException e) {
                 System.out.println("There seems to be a problem with the expression you put in.");
